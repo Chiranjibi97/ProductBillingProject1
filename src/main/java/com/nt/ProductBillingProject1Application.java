e165e0e6814ab39124fc3b641cccfb119897340b
Chiranjibi <Chiranjibidash1997@gmail.com>
2023-03-17 15:39:13 +0530

    Adding Date variable

--- src/main/java/com/nt/ProductBillingProject1Application.java
+++ src/main/java/com/nt/ProductBillingProject1Application.java
@@ -1,14 +1,18 @@
 package com.nt;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductBillingProject1Application {
 
+	private static Date date=new Date();
 	public static void main(String[] args) {
 		SpringApplication.run(ProductBillingProject1Application.class, args);
 		System.out.println("Chiranjibi");
+		System.out.println(date);
 	}
 
 }
